95adbe247e3c4dde24b317e444cbd200177b2825
jemshMarcel <jemshjms@gmail.com>
2025-02-27 17:37:45 +0300

    Add webhook route for validation with secret token

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/webhook', function (Request $request) {
+    // Validation du webhook
+    if ($request->hub_verify_token === 'mon_token_secret') {
+        return response($request->hub_challenge, 200);
+    }
+    return response('Token invalide', 403);
 });
